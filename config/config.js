75356e4279518ac912532251dc763ad46c182859
Miguel <miguel.cueva.l@uni.pe>
2021-06-26 22:21:35 -0500

    fix

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv').config();
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+process.env.DB_TYPE = process.env.DB_TYPE || 'mysql';
 process.env.DB_HOST = process.env.DB_HOST || 'localhost';
 process.env.DB_NAME = process.env.DB_NAME || 'sms';
 process.env.DB_USER = process.env.DB_USER || 'root';
